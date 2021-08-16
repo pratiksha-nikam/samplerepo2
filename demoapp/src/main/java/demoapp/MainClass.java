c67a61936e1498c280c03daa6f7b0bd9c1938432
DELL <Pratiksha>
2021-08-16 18:22:33 +0530

    first commit

---
+++ demoapp/src/main/java/demoapp/MainClass.java
@@ -0,0 +1,9 @@
+package demoapp;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!");	}
+
+}
---
+++ demoapp/src/main/java/demoapp/SalesClass.java
@@ -0,0 +1,12 @@
+package demoapp;
+
+public class SalesClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Car");
+		System.out.println("Nexa");
+		System.out.println("Ferrari");
+	}
+
+}

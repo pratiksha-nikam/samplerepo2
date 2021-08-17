99bc6797a554af0c4839a9baad6f724741047f50
DELL <Pratiksha>
2021-08-17 11:02:31 +0530

    Sales class modified

--- demoapp/src/main/java/demoapp/SalesClass.java
+++ demoapp/src/main/java/demoapp/SalesClass.java
@@ -4,9 +4,13 @@ public class SalesClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Car");
-		System.out.println("Nexa");
-		System.out.println("Ferrari");
+		int arr[]=new int[4];
+		arr[0]=10;arr[1]=20;arr[2]=40;arr[3]=60;
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+			
 	}
 
 }

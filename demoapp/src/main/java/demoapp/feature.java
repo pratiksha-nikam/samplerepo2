32167b8af48f32b368f9438c6efad686beeed3cc
DELL <Pratiksha>
2021-08-17 14:26:50 +0530

    get method added

--- demoapp/src/main/java/demoapp/feature.java
+++ demoapp/src/main/java/demoapp/feature.java
@@ -1,10 +1,15 @@
 package demoapp;
 
 public class feature {
+	public void get()
+	{
+		System.out.println("This is the get method");
+	}
 	public void display()
 	{
 		System.out.println("This is the feature class");
 	}
+	
 	
 
 }

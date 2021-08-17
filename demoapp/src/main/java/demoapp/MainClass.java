ec279888792d6eab4cec3dc3d6b95d88bceaa1dc
DELL <Pratiksha>
2021-08-17 13:49:28 +0530

    main class updated

--- demoapp/src/main/java/demoapp/MainClass.java
+++ demoapp/src/main/java/demoapp/MainClass.java
@@ -4,6 +4,9 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld!");	}
+		System.out.println("This is the main class");
+		System.out.println("Welcome to the main class");
+		
+		}
 
 }

6bdbb3a10648fefe9f3f86b704947c55f66fb7a7
DELL <Pratiksha>
2021-08-17 13:32:35 +0530

    sales updated

--- demoapp/src/main/java/demoapp/SalesClass.java
+++ demoapp/src/main/java/demoapp/SalesClass.java
@@ -6,9 +6,14 @@ public class SalesClass {
 		// TODO Auto-generated method stub
 		int arr[]=new int[4];
 		arr[0]=10;arr[1]=20;arr[2]=40;arr[3]=60;
+		int sum=0;
 		for(int i=0;i<arr.length;i++)
 		{
-			System.out.println(arr[i]);
+			//System.out.println(arr[i]);
+			sum=sum+arr[i];
+			System.out.println(sum);
+			
+			
 		}
 			
 	}
